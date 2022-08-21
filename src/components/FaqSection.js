c1678e689f0b74eeef33f12887e109f2213c35a6
sakhobiddin <saktuk12@gmail.com>
2022-08-21 19:29:00 +0200

    FaqSection is  done

---
+++ src/components/FaqSection.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Faqsection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>
+          Sizning brendiz ostida ishlasam bo`ladimi? Kompanyangizdagi
+          ishchilarga qanday imkonyatlar yaratilgan va oyliklari maximum qancha
+          to`lanadi?
+        </h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam,
+            labore nobis magni repellat est harum praesentium deserunt debitis
+            corrupti.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem ab maiores quos velit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Qaysi chet davlatlar bilan hamkorlikda ish yuritasiz?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam,
+            labore nobis magni repellat est harum praesentium deserunt debitis
+            corrupti.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem ab maiores quos velit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>
+          Sizning jamoangizga qo`shilishim uchun mendan qanday narsalar talab
+          qilinadi?
+        </h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam,
+            labore nobis magni repellat est harum praesentium deserunt debitis
+            corrupti.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem ab maiores quos velit.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>
+          O`z shaxsiy businessimni boshlashim uchun men avvalo nimadan
+          boshlashim kerak?
+        </h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quam,
+            labore nobis magni repellat est harum praesentium deserunt debitis
+            corrupti.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem ab maiores quos velit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqsection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 // import components
 import AboutSection from "../components/AboutSections";
 import BenefitsSection from "../components/BenefitsSection";
+import Faqsection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
       <BenefitsSection />
+      <Faqsection />
     </div>
   );
 };
